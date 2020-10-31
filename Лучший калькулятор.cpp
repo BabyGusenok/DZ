9372d6d00520670d3459e46a5f0fed63b6b6ce16
Маргарита <72893420+BabyGusenok@users.noreply.github.com>
2020-11-01 02:42:23 +0300

    Add files via upload

---
+++ Лучший калькулятор.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+typedef int Baby;
+typedef char Gusenok;
+Baby main()
+{
+	Baby a, b;
+	Gusenok znak;
+	cout << "ZNAK:";
+	cin >> znak;
+	cout << "Vvedi chislo 1 pupsik:" << endl;
+	cin >> a;
+	cout << "Vvedi chislo 2 pupsik:" << endl;
+	cin >> b;
+	switch (znak)
+	{case '+':
+		cout << a + b << endl;
+		return 0;
+	case '-':
+		cout << a - b << endl;
+		return 0;
+	case '*':
+		cout << a * b << endl;
+		return 0;
+	case '/':
+		cout << a / b << endl;
+		return 0; 
+	default:
+		cout << "Vvedite simvolllll" << endl;
+		return 0;
+	}
+
+
+}
