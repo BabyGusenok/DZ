95e7a0713c3af833e1d9c3e5431fdcb049ad96b3
Kirill <kirillbiktya@users.noreply.github.com>
2021-08-19 17:33:33 +0300

    Update Лучший калькулятор.cpp

--- Лучший калькулятор.cpp
+++ Лучший калькулятор.cpp
@@ -1,34 +1,67 @@
-﻿#include <iostream>
+#include <iostream>
+#include <regex>
+#include <cstdlib>
+#include <string>
+
 using namespace std;
 using Baby = int;
 using Gusenok = char;
+
+Baby get_int() {
+	string data_in;
+
+	cin >> data_in;
+	// экранировать нужно двумя обратынми слэшами, с++ немного странный, да
+	if (regex_match(data_in, regex("\\d{1,300}"))) { // мы должны работать и с огромными числами
+		return atoi(data_in.c_str());
+	}
+	else {
+		cout << "Vvedi chislo, a ne **shit**" << endl;
+		exit(1);
+	}
+}
+
+string get_operator() {
+	string data_in;
+
+	cin >> data_in;
+
+	if (regex_match(data_in, regex("[\\+\\-\\/\\*]"))) { // арифметические знаки являются управляющими символами в регулярках, экранируем
+		return data_in;
+	}
+	else {
+		cout << "Vvedi znak, a ne **shit**" << endl;
+		exit(1);
+	}
+}
+
 Baby main()
 {
 	Baby a, b;
-	Gusenok znak;
-	cout << "ZNAK:";
-	cin >> znak;
-	cout << "Vvedi chislo 1 pupsik:" << endl;
-	cin >> a;
-	cout << "Vvedi chislo 2 pupsik:" << endl;
-	cin >> b;
-	switch (znak)
-	{case '+':
-		cout << a + b << endl;
-		break;
-	case '-':
-		cout << a - b << endl;
-		break;
-	case '*':
-		cout << a * b << endl;
-		break;
-	case '/':
-		cout << a / b << endl;
-		break; 
-	default:
-		cout << "Vvedite simvolllll" << endl;
-		return 0;
+	string znak;
+	cout << "Vvedi chislo 1, pupsik: ";
+	a = get_int();
+	cout << "Vvedi znak, pupsik: ";
+	znak = get_operator();
+	cout << "Vvedi chislo 2, pupsik: ";
+	b = get_int();
+	switch (znak.c_str()[0]) // да простят меня боги c++ за это
+	{
+		case '+':
+			cout << a + b << endl;
+			break;
+		case '-':
+			cout << a - b << endl;
+			break;
+		case '*':
+			cout << a * b << endl;
+			break;
+		case '/':
+			cout << a / b << endl;
+			break;
+		default:
+			cout << "Vvedi znak, a ne **shit**" << endl;
+			return 1;
 	}
-
-
+	return 0;
 }
